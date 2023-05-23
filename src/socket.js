9c8460c109b791dcccac4b5c1a75a930b026b3e1
Panagiotis Tsionis <69398519+ptsionis@users.noreply.github.com>
2023-05-23 05:10:08 +0300

    process to import

--- src/socket.js
+++ src/socket.js
@@ -1,8 +1,5 @@
 import { io } from 'socket.io-client';
-import dotenv from "dotenv"
 
-dotenv.config();
-
-export const socket = io(process.env.URL, {
+export const socket = io(import.meta.URL, {
     autoConnect: false
 });
